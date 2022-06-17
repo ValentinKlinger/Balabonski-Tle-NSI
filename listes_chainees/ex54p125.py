3171b980cc329fa0ded46ec47aaebf78f42775d5
valentin <valentink.fr@gmail.com>
2022-06-17 18:32:10 +0200

    ajout de l'exercice 54 page 125

---
+++ listes_chainees/ex54p125.py
@@ -0,0 +1,17 @@
+# écrire une fonction identiques(l1, l2 qui renvoie un booléen indiquant si les listes l1 et l2 sont identiques, c'est-à-dire contiennent exactement les mêmes éléments, dans le même ordre. On supose que l'on peut comparer les éléménents de l1 et l2 avec l'égalité == de python.
+
+# ma réponse
+def identiques(l1, l2):
+	if l1 is None:
+		return l2 is None
+	if l2 is None:
+		return l1 is None
+	return l1.valeur == l2.valeur and identiques(l1.suivante, l2.suivante)
+
+# solution du manuel
+def identiques(l1, l2):
+	if l1 is None:
+		return l2 is None
+	if l2 is None:
+		return l1 is None
+	return l1.valeur == l2.valeur and identiques(l1.suivante, l2.suivante)
